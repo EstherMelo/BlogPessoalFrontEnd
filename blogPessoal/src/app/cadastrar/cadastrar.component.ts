b9a58439d740d6ac42d9689f212a3dfbcb5683b2
EstherMelo <emelo3172@gmail.com>
2021-11-10 02:57:47 -0300

    Consumo de API Login e Cadastro

--- blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
+import { AuthService } from '../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User} from '../model/User';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  confimarSenha: string 
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router 
+  ) { }
+
+  ngOnInit(): void { 
+    window.scroll(0,0)
   }
+
+  confirmSenha(event: any) {
+    this.confimarSenha = event.target.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(){
+    this.user.tipo = this.tipoUsuario
+    
+    if (this.user.senha != this.confimarSenha) {
+      alert('A senhas estão incorretas. ')
+    }else{
+      this.authService.cadastrar(this.user).subscribe((resp: User) =>{
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuario cadastrado com sucesso!')
+      })
+    }
+  }
+
 
 }
